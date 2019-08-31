a50009042ed12ce083ded8f93b28b669bb26bef6
Evan Kemp <ekp.web@gmail.com>
2019-08-30 19:20:29 -0700

    Allow spaces in file paths

--- get_file.py
+++ get_file.py
@@ -7,16 +7,21 @@ import file_operations
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description="Command to download a file from a user's bucket")
+    parser = argparse.ArgumentParser(description="Command to download a file from a user's bucket",
+                                     usage='get_file.py [-h] username password key local-file-path')
 
     parser.add_argument('username', help='Username for the user.')
     parser.add_argument('password', help='Password for the user.')
-    parser.add_argument('key', help='Key of the file on the s3 bucket.')
-    parser.add_argument('local_path', help='Location to save the file to.')
+    parser.add_argument('files', help='Must wrap each in quotes for spaces.', nargs='+', metavar='key local-file-path')
 
     args = parser.parse_args()
 
+    if len(args.files) > 2:
+        print('Invalid command. Files with spaces must be wrapped in quotes. '
+              'Usage: upload_file.py [-h] username password key local-path')
+        exit(1)
+
     user_functions.check_info(args.username, args.password)
     print('Downloading files...')
-    file_operations.download_file(args.username, args.key, args.local_path)
+    file_operations.download_file(args.username, args.files[0], args.files[1])
     print('Downloaded Files.')
--- upload_file.py
+++ upload_file.py
@@ -12,15 +12,22 @@ s3 = boto3.client('s3', region_name='us-west-2')
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
-        description="Uploads file to user's bucket, user has to have been created with create_user.py")
+        description="Uploads file to user's bucket, user has to have been created with create_user.py",
+        usage='upload_file.py [-h] username password key local-file-path'
+    )
     parser.add_argument('username', help='Username of the user')
     parser.add_argument('password', help='Password of the user')
-    parser.add_argument('file_key', help='Key of the file in the bucket. Must wrap in quotes for spaces.')
-    parser.add_argument('local_file', help='Local path of the file on system. Must wrap in quotes for spaces.')
+    parser.add_argument('files', help='Must wrap each in quotes for spaces.', nargs='+', metavar='key local-file-path')
+    # parser.add_argument('local_file', help='Local path of the file on system. Must wrap in quotes for spaces.')
 
     args = parser.parse_args()
 
+    if len(args.files) > 2:
+        print('Invalid command. Files with spaces must be wrapped in quotes. '
+              'Usage: upload_file.py [-h] username password key local-path')
+        exit(1)
+
     user_functions.check_info(args.username, args.password)
     print('Uploading file...')
-    file_operations.upload_file(args.username, args.local_file, args.file_key)
+    file_operations.upload_file(args.username, args.files[1], args.files[0])
     print('File Upload')
