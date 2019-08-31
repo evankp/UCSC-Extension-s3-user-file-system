b44ac6bf78a981693ff07dcc4742665ccbae1666
Evan Kemp <ekp.web@gmail.com>
2019-08-31 01:13:24 -0700

    Undo changes from last commit, with minor wording change.

--- get_file.py
+++ get_file.py
@@ -7,21 +7,16 @@ import file_operations
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description="Command to download a file from a user's bucket",
-                                     usage='get_file.py [-h] username password key local-file-path')
+    parser = argparse.ArgumentParser(description="Command to download a file from a user's bucket")
 
     parser.add_argument('username', help='Username for the user.')
     parser.add_argument('password', help='Password for the user.')
-    parser.add_argument('files', help='Must wrap each in quotes for spaces.', nargs='+', metavar='key local-file-path')
+    parser.add_argument('key', help='Key of the file in the bucket. Must wrap in quotes for spaces.')
+    parser.add_argument('local_file', help='Local path of the file on system. Must wrap in quotes for spaces.')
 
     args = parser.parse_args()
 
-    if len(args.files) > 2:
-        print('Invalid command. Files with spaces must be wrapped in quotes. '
-              'Usage: upload_file.py [-h] username password key local-path')
-        exit(1)
-
     user_functions.check_info(args.username, args.password)
     print('Downloading files...')
-    file_operations.download_file(args.username, args.files[0], args.files[1])
+    file_operations.download_file(args.username, args.key, args.local_file)
     print('Downloaded Files.')
